6b7160408ce9d417f48c3e168d6aab0197d7acf5
JayJay1024 <lygm1062@gmail.com>
2024-03-28 15:29:03 +0800

    refactor: remove name config

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   themeConfig: {
-    name: 'documents',
     footer: `<br>Copyright © ${(/* @__PURE__ */ new Date()).getFullYear()} | Powered by <a href="https://helixbridge.app" target="_blank" rel="noreferrer">helix</a>`,
     logo: "/logoLight.svg",
     nav: [],
