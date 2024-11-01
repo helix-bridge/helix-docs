e7c0db4b672d48cfa685693080501df2e5f3087c
xiaoch05 <xiaoch2010@gmail.com>
2024-11-01 09:50:49 +0800

    update logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,7 +51,7 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     navbar: {
-      title: "Helixbox",
+      title: "",
       logo: {
         alt: "Helix Logo",
         src: "img/logo.svg",
